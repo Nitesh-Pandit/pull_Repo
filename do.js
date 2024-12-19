51b6594693cd6cab03ebd0732b6b3f8a3ade7569
pp5928976 <npandit669@rku.ac.in>
2024-12-19 23:35:22 +0530

    again chnage somthing

---
+++ do.js
@@ -0,0 +1,5 @@
+// Node.js script to print your name
+const name = "Your Name"; // Replace "Your Name" with your actual name
+console.log(`My name is ${name}.`);
+console.log("My name is nitsh from nepal");
+console.log("heollo all this is nitesh form your inida");
