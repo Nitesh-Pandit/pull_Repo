3f0a9f987909e3f6780eb298ec81f97ded433744
pp5928976 <npandit669@rku.ac.in>
2024-12-19 23:38:52 +0530

    again commit

--- do.js
+++ do.js
@@ -3,3 +3,5 @@ const name = "Your Name"; // Replace "Your Name" with your actual name
 console.log(`My name is ${name}.`);
 console.log("My name is nitsh from nepal");
 console.log("heollo all this is nitesh form your inida");
+console.log("heollo all this is");
+console.log("My name is nitsh form Austrialia");
